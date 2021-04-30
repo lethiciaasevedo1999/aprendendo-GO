05b081335291d901259674583ba5515609d7aec9
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2021-04-30 06:30:36 -0300

    Forma diferente de declara a variavel

---
+++ funcao1.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"curso-go/math"
+)
+
+func main() {
+	resultado := math.Soma(1, 1)
+	fmt.Printf("%v", resultado)
+
+}
+
+//Forma de declarar função
+// func Soma (valor tipo, valor tipo) tipo que vai retornar na função
