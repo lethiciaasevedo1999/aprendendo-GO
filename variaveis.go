e67f7fd8d1b066591452fbeaf6eb09fe6d8a9012
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2021-04-30 05:58:00 -0300

    aprendendo sobre variaveis

---
+++ variaveis.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+//Declaração de variável - estamos dizendo que ela existe.
+//var a string
+
+func main(){
+
+	// Atribuição de variável - estamos dando um valor a ela.
+	//a = "Lethicia"
+
+
+	//- Declarar e atribuir
+	a := 10
+	b := "World"
+	c := 3.144
+	d := false
+	e := `uoooooou legaaaal uhul`
+
+	fmt.Printf("%v \n" , a) //%T - mostra o tipo da variável
+	fmt.Printf("%v \n" , b)
+	fmt.Printf("%v \n" , c)
+	fmt.Printf("%v \n" , d)
+	fmt.Printf("%v \n" , e)
+
+
+}
